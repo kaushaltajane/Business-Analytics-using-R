c93d48384cec9229a0476a33d71fa2f4cd6f8dc1
kaushaltajane <65940597+kaushaltajane@users.noreply.github.com>
2020-06-07 14:09:47 +0530

    Create Task 4B.R

---
+++ Task 4B.R
@@ -0,0 +1,11 @@
+mean(X_ities_short$Price)
+
+z <- mean(X_ities_short$Price)
+
+arrange(X_ities_short, Price)
+
+arrange(X_ities_short, mean(X_ities_short$Price))
+
+head(arrange(X_ities_short,Price), n = 5)
+
+xyz <- head(arrange(X_ities_short,Price), n = 5)
