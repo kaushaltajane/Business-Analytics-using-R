5b485db194669a168d67d160b933b8970fa78adc
kaushaltajane <65940597+kaushaltajane@users.noreply.github.com>
2020-06-07 14:07:30 +0530

    Update Task 2A.R

--- Task 2A.R
+++ Task 2A.R
@@ -7,3 +7,14 @@ mean(ities_short)
 x <- mean(ities_short)
 
 ities_short <- ities_short %>% mutate(LineItem_LongName = ifelse(length(LineItem) > x),1,0)
+
+ggplot(gRx9MlZTSlOcfTJWU3pTqA_f794a300098c424ebb4efea245153be4_ities_short) +
+
+aes(x = Department, y = LineItem_LongName) +
+
+geom_boxplot(fill = "#0c4c8a") +
+
+labs(y = "LineItem_LongName") +
+
+theme_minimal()
+
