a8eb48f863ee80391c7aa1768a29e9a0f04ba0a7
kaushaltajane <65940597+kaushaltajane@users.noreply.github.com>
2020-06-07 14:11:00 +0530

    Update Task 1B.R

--- Task 1B.R
+++ Task 1B.R
@@ -1,3 +1,6 @@
+#Explain what approach would you use to handle them? (you may ignore missing values in a categorical/ factor variable) 
+
+
 library(tidyr)
 
 sum(is.na(gRx9MlZTSlOcfTJWU3pTqA_f794a300098c424ebb4efea245153be4_ities_short$TotalDue))
